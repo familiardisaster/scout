2a40fc0b818621c482440f1f6a879119f2931509
Russ <116748806+familiardisaster@users.noreply.github.com>
2023-02-12 12:03:10 -0500

    Delete fuzzmagic.py

--- fuzzmagic.py
+++
@@ -1,6 +0,0 @@
-with open("/home/ubuntu/wordlists/$2/subs.txt",'r') as infile, open("/home/ubuntu/wordlists/ffuf.txt",'a+') as outfile:
-        for line in infile:
-            words = line.split('.')
-            for word in words:
-                outfile.write('/' + word)
-
