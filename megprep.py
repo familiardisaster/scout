d4da347b6bb2eac9d844ffa8f904b53cdac2aa75
Russ <116748806+familiardisaster@users.noreply.github.com>
2023-02-12 12:03:14 -0500

    Delete megprep.py

--- megprep.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-path = argv[1]
-
-with open("/home/ubuntu/wordlists/" + path + "/subs.txt",'r') as infile, open('/home/ubuntu/wordlists/meg.txt','w') as megfile:
-    for sub in infile:
-        megfile.write('https://'+sub)
